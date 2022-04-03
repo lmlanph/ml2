0231b42f91ca2a404942f24cdffe83e142c0e894
lmlanph <mxm.ml@Macs-MacBook-Air.local>
2022-04-03 10:21:44 -0700

    remove br

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,12 +6,10 @@ function About() {
 
         <div className="about-container">
 
-            <p className="about">Howdy!&nbsp;&nbsp;My name is Mac. I like to take pictures, draw things, animate stuff, maybe even write some code. I made this site to practice react&nbsp;js<br/><span>it's a work in&nbsp;progress</span>&nbsp;😅</p>
-            <br/>
-            <br/>
+            <p className="about">Howdy!&nbsp;&nbsp;My name is Mac. I like to take pictures, draw things, animate stuff, maybe even write some code. I made this site to practice react&nbsp;js<br /><span>it's a work in&nbsp;progress</span>&nbsp;😅</p>
 
             <img className="about-image" src='./images/mac.jpeg'/>
-            <br/><br/>
+            <br /><br />
         </div>
 
     )
